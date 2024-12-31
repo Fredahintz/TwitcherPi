a7bca2bef2951de94b927fd857386adc5d67c1ea
Fred Hintz <7254410+Fredahintz@users.noreply.github.com>
2024-12-31 09:04:36 -0500

    thought

--- dal2.py
+++ dal2.py
@@ -1,40 +1,73 @@
-from pymongo import MongoClient
-from io import BytesIO
-from datetime import datetime
+import os
 
-client = MongoClient('mongodb://192.168.1.226')
-db = client.twicherpi
+from sqlalchemy import create_engine, Column, Integer, String, TIMESTAMP, LargeBinary
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 
-class ImageDocument():
-    __image = BytesIO
-    __captured_date = ""
-    __author = ""
+from dotenv import load_dotenv
 
-    def __init__(self):
-        now = datetime.now()
-        self.__captured_date = now
+load_dotenv()
 
-    def seed_database(self):
-        labels = ['sparrow', 'blackbird','robin']
+# Database connection
+#yikes - postgres uses a lot of memory on the pi. I'm going to use sqlite or Tinydb instead for now
+postgres_password = os.getenv('POSTGRES_PASSWORD')
 
-        for label in labels:
-            db.labels2.insert_one({'label':label})
+DATABASE_URL = f'postgresql://postgres:{postgres_password}@192.168.0.171/birdwatcherdb'
+engine = create_engine(DATABASE_URL)
+Session = sessionmaker(bind=engine)
+session = Session()
 
-    def get_labels(self):
-        """ Returns a list of labels """
-        data = list(db.labels2.find({},{"_id":False}))
-        print(data)
-        return data
+Base = declarative_base()
 
-    def get_ids(self):
-        id_list = [str(id) for id in db.labels2.find().distinct('_id')]
-        return id_list
+class Image(Base):
+    __tablename__ = 'images'
+    __table_args__ = {'schema': 'postgres'}
 
-    def save_image(self, image:BytesIO):
-        self.__image = image.getvalue
-        myimage = image.getvalue()
-        image_id = db.images2.insert_one({"date":self.__captured_date, "image":myimage, "author":self.__author})
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    date_created = Column(TIMESTAMP, nullable=False)
+    image = Column(LargeBinary, nullable=False)
+    label = Column(String(250), nullable=False)
 
-# temporary routine
-im = ImageDocument()
-im.get_labels()
+# Create the table
+Base.metadata.create_all(engine)
+
+# from pymongo import MongoClient
+# from io import BytesIO
+# from datetime import datetime
+
+# client = MongoClient('mongodb://192.168.1.226')
+# db = client.twicherpi
+
+# class ImageDocument():
+#     __image = BytesIO
+#     __captured_date = ""
+#     __author = ""
+
+#     def __init__(self):
+#         now = datetime.now()
+#         self.__captured_date = now
+
+#     def seed_database(self):
+#         labels = ['sparrow', 'blackbird','robin']
+
+#         for label in labels:
+#             db.labels2.insert_one({'label':label})
+
+#     def get_labels(self):
+#         """ Returns a list of labels """
+#         data = list(db.labels2.find({},{"_id":False}))
+#         print(data)
+#         return data
+
+#     def get_ids(self):
+#         id_list = [str(id) for id in db.labels2.find().distinct('_id')]
+#         return id_list
+
+#     def save_image(self, image:BytesIO):
+#         self.__image = image.getvalue
+#         myimage = image.getvalue()
+#         image_id = db.images2.insert_one({"date":self.__captured_date, "image":myimage, "author":self.__author})
+
+# # temporary routine
+# im = ImageDocument()
+# im.get_labels()
